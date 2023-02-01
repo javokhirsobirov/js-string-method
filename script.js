5dace9d9986657e88a544d95f3a31b347eae8003
javokhirsobirov <dilya.husenova11.78@gmail.com>
2023-02-01 09:32:44 +0500

    homework

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-//TASK 1
-// 1) let user = prompt('What is your Name ?') // ismi faqatgina a/A harfi bilan boshlanadigan odamlarni kirsin.
-// 2) let balance = prompt('How much do you have money ?') // Puli 10 000 mingdan kam bo'lmaganlarni kiriting.
-// 3) let account = prompt('Code ?') // 7777 bo'lishi kerak;
-// 4) let cash = prompt('How much do you want to cash ?') //qancha puli qoldi va qancha naqt pul oldi log da chiqarilsin;
+//Task 1
+// 1)let username = propmt('What is your Name ?') // if the length of username is grater than 6 let them to enter the club or if the length of username is smaller than 6 don't let them to enter!
+// 2)let people = prompt('How many you are ?') // How many they are and there shuld be a limit no more 10 persons;
+// 3)let money = prompt('We have rule, you need to pay 1000 per each') ' Every person shuld pay 1000 for entering
+// 4)let order = prompt('Do you need order something ?') // The shuld order something and you need to check if the thing is available in restoran;
 
-//TASK 2
-// let txt = "Lorem ipsum@ dolor sit@ amet consectetur, adipisicing@ elit. Aliquam lorem molestias consequuntur! alexandr .Atque@ lorem adipisci delectus@ lorem facere accusantium! Aliquam voluptate lorem aut asperiores@ fugiat sunt lorem unde necessitatibus@ delectus!"
+let username = prompt("What is your Name ?")
 
-// 1) txt dagi hamma @ larni o'chirib chiqush;
-// 2) txt dagi hamma lorem so'zini 'Hello' ga o'zgartirasizlar;
-// 3) txt dagi hamma lorem so'zini nechi marta qaytariliyapti sanaysizlar;
-// 4) txt dagi Alexandr so'zini topib uni bosh harfini katta qilasizlar;
+if (username.length >= 6) {
+    let people = +prompt('How many you are ?')
+    if (people >= 10) {
+        console.log('Shumon mul a 10 kas kam shugeton darkor');
+    } else {
+        let money = prompt('We have rule, you need to pay 1000 per each')
+        let sum = people * 1000
+        console.log(sum);
+
+    }
+
+    console.log('Tuya nomata harfash a 6 ta kam');
+
+    let order = shashlik;
+    order = prompt('Do you need order something ?')
+    if (order = shashlik || order > shashlik) {
+        console.log('order, order5');
 
 
-// let user = ('Javoxir')
-// console.log(user);
-// let balance = prompt('puli yoqlar kirmasin')
-// console.log(balance);
-// let account = prompt('kodni kiriting')
-// let cash = prompt('qancha pulingiz qoldi?')
+    }
 
-
-let txt = "Lorem ipsum@ dolor sit@ amet consectetur, adipisicing@ elit. Aliquam lorem molestias consequuntur! alexandr .Atque@ lorem adipisci delectus@ lorem facere accusantium! Aliquam voluptate lorem aut asperiores@ fugiat sunt lorem unde necessitatibus@ delectus!"
-// console.log(txt.replace(/@/gi, ''));
-// console.log(txt.replace(/lorem/gi, 'Hello'));
-// console.log(txt.match(/lorem/gi))
-console.log(txt.replace("alexandr", 'Alexandr'));
